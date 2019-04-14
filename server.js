4f366e3c5ec5160e42b06960beda372f5a2ad430
TheAlexBig <00062816@uca.edu.sv>
2019-04-14 08:17:39 -0600

    Heroku fix port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http");
 
 const app = require("./app");
 
-const PORT = process.env.API_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 
 const server = http.createServer(app);
